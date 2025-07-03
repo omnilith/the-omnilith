32297207afc1a5567d0a18043e80a4d3524e6b75
Tim Butler <timbutler@Tims-MacBook-Pro.local>
2025-07-03 15:19:18 -0400

    ssl

--- core/db.ts
+++ core/db.ts
@@ -2,6 +2,9 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export async function query(text: string, params?: unknown[]) {
