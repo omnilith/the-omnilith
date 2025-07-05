a4b6f915ce50ed9fdcdca0a7983fdbedaf4ef24f
Tim Butler <timbutler@Tims-MacBook-Pro.local>
2025-07-05 16:27:25 -0400

    more auth

--- world/components/Nav.tsx
+++ world/components/Nav.tsx
@@ -1,27 +1,34 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./Nav.module.css";
+import { createClient } from "@core/core/utils/supabase/server";
 
 const navItems = [
   { label: "Home", href: "/" },
   { label: "Blog", href: "/view/view-postListView" },
 ];
 
-const Nav: React.FC = () => (
-  <nav className={styles.mainNav}>
-    <div>
-      <Link href="/" className={styles.mainLogo}>
-        <span>Omnilith</span>
-      </Link>
-    </div>
-    <div>
-      {navItems.map((item) => (
-        <Link key={item.href} href={item.href} className={styles.rightLink}>
-          {item.label}
-        </Link>
-      ))}
-    </div>
-  </nav>
-);
+export default async function Nav() {
+  const supabase = await createClient();
 
-export default Nav;
+  const { data, error } = await supabase.auth.getUser();
+  console.log("User data:", data, "Error:", error);
+
+  return (
+    <nav className={styles.mainNav}>
+      <div>
+        <Link href="/" className={styles.mainLogo}>
+          <span>Omnilith</span>
+        </Link>
+        {/* {data?.user ? data.user.email : "Not logged in"} */}
+      </div>
+      <div>
+        {navItems.map((item) => (
+          <Link key={item.href} href={item.href} className={styles.rightLink}>
+            {item.label}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+}
